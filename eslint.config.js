91c9b97a67fe452de73b6b834aed8560021accc9
Artem Elkin <artelkin@gmail.com>
2025-04-09 00:21:32 +0200

    Use the eslint plugin; keywords

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-//import actc39 from 'eslint-plugin-async-context-tc39';
+import actc39 from 'eslint-plugin-async-context-tc39';
 
 export default tseslint.config(
     { ignores: ['dist/'] },
     eslint.configs.recommended,
     tseslint.configs.recommended,
-    //actc39.configs['flat/recommended'],
+    actc39.configs['flat/recommended'],
 );
